b412e78e55e28bc991bd794a15fbc9817810a30e
Gabriel Guimarães <gabrielglfg@outlook.com>
2022-01-12 19:05:54 -0300

    added framework

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RAUSS</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+
+    <!-- Magnific Popup core CSS file -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css"
+        integrity="sha512-WEQNv9d3+sqyHjrqUZobDhFARZDko2wpWdfcpv44lsypsSuMO0kHGd3MQ8rrsBn/Qa39VojphdU6CMkpJUmDVw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/header-sm.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/banner.css">
+    <link rel="stylesheet" href="assets/css/about.css">
+    <link rel="stylesheet" href="assets/css/valores.css">
+    <link rel="stylesheet" href="assets/css/portfolio.css">
+    <link rel="stylesheet" href="assets/css/contact.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+
+</head>
+
+<body>
+    <header class="header-lg">
+        <nav class="main-header container">
+            <div class="header-left">
+                <div class="logo">
+                    <a href="/">
+                        <img src="assets/images/logo_white.svg" height="50" alt="Rauss Logo">
+                    </a>
+                </div>
+            </div>
+            <div class="header-right">
+                <div class="menuItems">
+                    <ul>
+                        <li class="active"><a href="#bannerInitial">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#portfolio">Portfolio</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </nav>
+    </header>
+
+    <header id="header" class="nav-down header-sm">
+        <div class="wrap">
+          <div class="logo">
+            <a href="/">
+                <img src="assets/images/logo_white.svg" height="50" alt="Rauss Logo">
+            </a>
+          </div>
+          <a class="hamburger">
+            <div class="line top"></div>
+            <div class="line middle"></div>
+            <div class="line bottom"></div>
+          </a>
+        </div>
+      </header>
+      <div id="menu-wrap">
+        <div class="stripe"></div>
+          <nav class="navigation">
+              <ul>
+                  <li>
+                      <a href="/">
+                          <span data-text="Home"></span>
+                          Home
+                      </a>
+                  </li>
+                  <li>
+                      <a href="#about">
+                          <span data-text="About"></span>
+                          About
+                      </a>
+                  </li>
+                  <li>
+                      <a href="#portfolio">
+                          <span data-text="Portfólio"></span>
+                          Portfólio
+                      </a>
+                  </li>
+                  <li>
+                    <a href="#contact">
+                        <span data-text="Contato"></span>
+                        Contato
+                    </a>
+                </li>
+              </ul>
+          </nav>
+      </div>
+    <section id="bannerInitial">
+        <div class="bg-1"></div>
+        <div class="container">
+            <div class="banner-inner">
+                <h1>Rauss</h1>
+                <p>Construindo o futuro junto com você</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="about">
+        <div class="container">
+            <div class="card-about">
+                <div class="row w-100 mx-auto justify-content-center">
+                <h1 class="section-title-maroon  text-center">Sobre a empresa.</h1>
+                    <div class="col-lg-10 ">
+                        Rauss é uma referência a BAUHAUS uma das mais importantes Escolas de Design e Arquitetura do
+                        século
+                        XX. Era também um grupo de pessoas vanguardistas, ou seja, que estavam à frente do seu tempo,
+                        inovando sempre em ideias, tendências e opiniões.
+                        A sua ideologia era a do funcionalismo e racionalismo. O funcionalismo é importante pois os
+                        materiais usados nas construções tendem a ter um porquê claro e racional.
+                        Em alemão, Haus significa Casa.
+                        Com o emprego do ”R” queremos trazer a força sonora que esta letra dá as palavras e com o duplo
+                        ”SS”
+                        a modernidade e inovação que o emprego de duas consoantes repetidas proporciona.
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section id="values">
+        <div class="container">
+            <div class="row gx-0">
+                <div class="col-lg-4">
+                    <div class="cardWrapper bg-value1">
+                        <div class="card">
+                            <div class="cardFace front">
+                                <div class="bg-1"></div>
+                                <h1>Missão</h1>
+                            </div>
+                            <div class="cardFace back">
+                                <div class="bg-1"></div>
+                                <p>Fazer diferente! Explorar novas tecnologias e acessibilizar o consumo e o
+                                    investimento no setor, beneficiando e realizando os sonhos dos nossos clientes.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="cardWrapper bg-value2">
+                        <div class="card">
+                            <div class="cardFace front">
+                                <div class="bg-1"></div>
+                                <h1>Visão</h1>
+                            </div>
+                            <div class="cardFace back">
+                                <div class="bg-1"></div>
+                                <p>Ser reconhecida como realizadora em inovação e tecnologia aplicadas a redução de
+                                    custos em construções no Brasil. Ser referência nacional em investimentos seguros,
+                                    éticos e democráticos no mercado imobiliário.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="cardWrapper bg-value3">
+                        <div class="card">
+                            <div class="cardFace front">
+                                <div class="bg-1"></div>
+                                <h1>Valores</h1>
+                            </div>
+                            <div class="cardFace back">
+                                <div class="bg-1"></div>
+                                <p>Honestidade, transparência, ousadia, segurança, comprometimento, clareza, agilidade,
+                                    confiança, credibilidade, sustentabilidade, qualidade, rentabilidade, relacionamento
+                                    de amizade, valorizção do indivíduo</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section id="portfolio">
+        <div class="grid-portfolio">
+            <div class="imagem-1">
+                <a href="/assets/images/portfolio/imagem-1.jpeg" class="without-caption image-link">
+                    <img src="/assets/images/portfolio/imagem-1.jpeg" alt="Portfolio">
+                </a>
+            </div>
+            <div class="imagem-2">
+                <a href="/assets/images/portfolio/imagem-2.jpeg" class="without-caption image-link">
+                    <img src="/assets/images/portfolio/imagem-2.jpeg" alt="Portfolio">
+                </a>
+            </div>
+            <div class="imagem-3">
+                <a href="/assets/images/portfolio/imagem-3.jpeg" class="without-caption image-link">
+                    <img src="/assets/images/portfolio/imagem-3.jpeg" alt="Portfolio">
+                </a>
+            </div>
+            <div class="imagem-4">
+                <a href="/assets/images/portfolio/imagem-4.jpeg" class="without-caption image-link">
+                    <img src="/assets/images/portfolio/imagem-4.jpeg" alt="Portfolio">
+                </a>
+            </div>
+            <div class="imagem-5">
+                <a href="/assets/images/portfolio/imagem-5.jpeg" class="without-caption image-link">
+                    <img src="/assets/images/portfolio/imagem-5.jpeg" alt="Portfolio">
+                </a>
+            </div>
+            <div class="imagem-6">
+                <a href="/assets/images/portfolio/imagem-6.jpeg" class="without-caption image-link">
+                    <img src="/assets/images/portfolio/imagem-6.jpeg" alt="Portfolio">
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <section id="contact">
+        <div class="container">
+            <div class="row mx-auto w-100 justify-content-center">
+                <div class="contact-title">
+                    <h1 class="section-title">Contato</h1>
+                </div>
+                <form>
+                    <div class="row mx-auto w-100 justify-content-center">
+                        <div class="col-md-7">
+                            <label for="name" class="form-label">Nome:</label>
+                            <input type="text" class="form-control" id="name" required>
+                        </div>
+                        <div class="col-md-7">
+                            <label for="phone" class="form-label">Telefone:</label>
+                            <input type="text" class="form-control" id="phone" required>
+                        </div>
+                        <div class="col-md-7">
+                            <label for="email" class="form-label">E-mail:</label>
+                            <input type="text" class="form-control" id="email" aria-describedby="inputGroupPrepend2"
+                                required>
+                        </div>
+                        <div class="col-12 justify-content-center mt-5 d-flex">
+                            <button class="btn btn-contact" type="submit">Enviar</button>
+                        </div>
+                    </div>
+
+                </form>
+
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="row justify-content-center align-itmes-center">
+            <div class="col-lg-4 col-sm-12 footer-img-link sm-mb-5">
+                <a href="#home">
+                    <img src="./assets/images/logo_white.svg" alt="Rauss Logo">
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-12 sm-mb-5">
+                <ul class="list-unstyled">
+                    <li><a href=""><i class="fab fa-facebook-square"></i></a></li>
+                    <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                    <li><a href=""><i class="fab fa-whatsapp"></i></a></li>
+                    <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                </ul>
+            </div>
+            <div class="col-lg-4 col-sm-12 sm-mb-5">
+                <span>2021 - Direitos Reservados</span>
+            </div>
+
+        </div>
+
+    </footer>
+
+    <!-- JQUERY MIN -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <!-- jQuery 1.7.2+ or Zepto.js 1.0+ -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+    <!-- Magnific Popup core JS file -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"
+        integrity="sha512-IsNh5E3eYy3tr/JiX2Yx4vsCujtkhwl7SLqgnwLNgf04Hrt9BT9SXlLlZlWx+OK4ndzAoALhsMNcCmkggjZB1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="./assets/js/header.js"></script>
+    <script src="./assets/js/gsap.min.js"></script>
+    <script src="./assets/js/portfolioImageZoom.js"></script>
+    <script src="./assets/js/headerMobile.js"></script>
+    <script>
+        gsap.set(".cardWrapper", {
+            perspective: 800
+        });
+        gsap.set(".card", {
+            transformStyle: "preserve-3d"
+        });
+        gsap.set(".back", {
+            rotationY: -180
+        });
+        gsap.set([".back", ".front"], {
+            backfaceVisibility: "hidden"
+        });
+
+        $(".cardWrapper").hover(
+            function () {
+                gsap.to($(this).find(".card"), {
+                    duration: 1.2,
+                    rotationY: 180,
+                    ease: Back.easeOut
+                });
+            },
+            function () {
+                gsap.to($(this).find(".card"), {
+                    duration: 1.2,
+                    rotationY: 0,
+                    ease: Back.easeOut
+                });
+            }
+        );
+
+        //a nice little intro;
+        gsap.to($(".card"), {
+            duration: 1,
+            rotationY: -180,
+            repeat: 1,
+            yoyo: true,
+            stagger: 0.1
+        });
+    </script>
+</body>
+
+</html>
